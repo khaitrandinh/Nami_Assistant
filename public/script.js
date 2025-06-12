146602077b3e74bb56c363a7edef34eb31d5706d
JamesTran <khaidinhtran123@gmail.com>
2025-06-12 16:20:43 +0700

    Fix/domain_vercel

--- public/script.js
+++ public/script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
         addMessage('ai', '<span class="loading">Đang suy nghĩ...</span>'); // Hiển thị trạng thái loading
 
         try {
-            const response = await fetch('https://nami-assistant.vercel.app//ask-assistant', {
+            const response = await fetch('https://nami-assistant.vercel.app/ask-assistant', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
