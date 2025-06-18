16a68151007c9e36e4e95bca2cd1afb9db3c4a11
JamesTran <khaidinhtran123@gmail.com>
2025-06-18 14:38:45 +0700

    fix

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.post('/ask-assistant', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
